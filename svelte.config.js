372fe6ca1d18d266209be51e76d7c5432cd19496
marahmanjs <imarahmandev@gmail.com>
2022-01-22 14:31:43 +0500

    static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
+
 const config = {
 	kit: {
+		// adapter: adapter(),
 		adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
