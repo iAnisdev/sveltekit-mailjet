2eb640938b821969751bab780411600849378ba2
marahmanjs <imarahmandev@gmail.com>
2022-01-24 23:14:19 +0500

    setup email basic

--- functions/send.js
+++ functions/send.js
@@ -1,8 +1,23 @@
-export async function onRequestPost({env , data}) {
-    const info = JSON.stringify({
-        env,
-        message: 'response from test function',
-        data
-      })
-      return new Response(info, null, 2);
-    }
+const mailjet = require('node-mailjet').connect('45a3af7f2d4e8e848bebe8f97b960331', '1da0d3c35c170485c24b8f9ac3bcf275')
+export async function onRequestPost(context) {
+  const { data } = context;
+  mailjet.post("send", { 'version': 'v3.1'}).request({
+    "Messages": [{
+      "From": {
+        "Email": "pilot@mailjet.com",
+        "Name": "Mailjet Pilot"
+      },
+      "To": [{
+        "Email": data.email,
+        "Name": data.name
+      }],
+      "Subject": data.subject,
+      "TextPart": "Following is a Test Email!",
+      "HTMLPart": `<h3>Dear ${data.name}, welcome to my App, Your IP is ${data.ip}`
+    }]
+  }).then((result) => {
+    return new Response(JSON.stringify(result.body), null, 2);
+  }).catch((err) => {
+    return new Response(err, null, 2);
+  })
+}
