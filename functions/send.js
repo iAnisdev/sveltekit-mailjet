d5b2ff8c89543d4d59ed5fb301ad3eb3a8ef4dc1
marahmanjs <imarahmandev@gmail.com>
2022-01-24 22:56:26 +0500

    udpated envs

--- functions/send.js
+++ functions/send.js
@@ -1,7 +1,7 @@
 export async function onRequestGet(context) {
     const info = JSON.stringify({
         env,
-        message: 'response from function'
+        message: 'response from test function'
       })
       return new Response(info, null, 2);
     }
