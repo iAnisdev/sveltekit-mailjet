e9cc411e2923251d61ba5881013e6405a8b94662
marahmanjs <imarahmandev@gmail.com>
2022-02-03 10:26:45 +0500

    status update

--- functions/test.js
+++ functions/test.js
@@ -2,7 +2,7 @@ export async function onRequestGet({env}) {
     const info = JSON.stringify({
         message: 'hurrah it works',
         platform: env,
-        'Authorization': 'Basic ' + btoa(`${env.API_KEY}:${env.API_SECRET}`)
+        authorization: 'Basic ' + btoa(`${env.API_KEY}:${env.API_SECRET}`)
     });
     return new Response(info, null, 2);
     
