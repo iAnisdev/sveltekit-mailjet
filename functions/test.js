3aabc473b509fb2eb50b7bcf8c5cb86bdabf32ea
marahmanjs <imarahmandev@gmail.com>
2022-02-03 10:21:24 +0500

    btoa check

--- functions/test.js
+++ functions/test.js
@@ -1,7 +1,8 @@
 export async function onRequestGet({env}) {
     const info = JSON.stringify({
         message: 'hurrah it works',
-        platform: env
+        platform: env,
+        'Authorization': 'Basic ' + btoa(`${env.API_KEY}:${env.API_SECRET}`)
     });
     return new Response(info, null, 2);
     
